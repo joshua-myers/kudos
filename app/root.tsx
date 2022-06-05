959b79e0d6c28fb41ff40e7c529d265c15ec4f88
joshua-myers <josh.myers@headway.io>
2022-06-05 16:37:05 +0000

    add tailwind css

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,23 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "@remix-run/react";
+import styles from './styles/app.css';
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export default function App() {
   return (
