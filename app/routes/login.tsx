293aa58678473c14c1294b405659b7ad94893661
joshua-myers <josh.myers@headway.io>
2022-06-05 19:16:31 +0000

    add layout component

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,9 @@
+import { Layout } from '~/components/layout'
+
 export default function Login() {
-    return (
-      <div className="h-screen flex justify-center items-center">
-        <h2 className="text-yellow-300 font-extrabold text-5xl">Login Route</h2>
-      </div>
-    )
-  }
+  return (
+    <Layout>
+      <h2 className="text-yellow-300 font-extrabold text-5xl">Login Route</h2>
+    </Layout>
+  )
+}
