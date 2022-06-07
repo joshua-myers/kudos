affdd27cfc6d732b13fb976afa2bec0d2533968d
joshua-myers <josh.myers@headway.io>
2022-06-07 03:45:45 +0000

    add type for login

--- app/utils/types.server.ts
+++ app/utils/types.server.ts
@@ -1,3 +1,5 @@
 import { User } from "@prisma/client"
 
-export type RegisterForm = Pick<User, 'email' | 'password'> & Pick<User['profile'], 'firstName' | 'lastName'>
+export type RegisterForm = Pick<User, 'email' | 'password'> & Pick<User['profile'], 'firstName' | 'lastName'>;
+
+export type LoginForm = Pick<User, 'email' | 'password'>;
