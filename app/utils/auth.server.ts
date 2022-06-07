51fce16ceb113fed352595b3496f4f5f2f2719ec
joshua-myers <josh.myers@headway.io>
2022-06-07 04:02:09 +0000

    add session storage

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/node'
+import { createCookieSessionStorage, json, redirect } from '@remix-run/node'
 import bcrypt from 'bcryptjs'
 import { prisma } from './prisma.server'
 import type { LoginForm, RegisterForm } from './types.server'
@@ -20,6 +20,8 @@ export const register = async (user: RegisterForm) => {
       { status: 400 },
     )
   }
+
+  return createUserSession(newUser.id, '/');
 }
 
 export async function login({ email, password }: LoginForm) {
@@ -30,5 +32,34 @@ export async function login({ email, password }: LoginForm) {
   if (!user || !(await bcrypt.compare(password, user.password)))
     return json({ error: `Incorrect login` }, { status: 400 })
 
-  return { id: user.id, email }
+  return createUserSession(user.id, "/");
+}
+
+
+const sessionSecret = process.env.SESSION_SECRET
+if (!sessionSecret) {
+  throw new Error('SESSION_SECRET must be set')
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: 'kudos-session',
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [sessionSecret],
+    sameSite: 'lax',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+})
+
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession()
+  session.set('userId', userId)
+  return redirect(redirectTo, {
+    headers: {
+      'Set-Cookie': await storage.commitSession(session),
+    },
+  })
 }
