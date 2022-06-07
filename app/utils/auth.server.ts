d6ea2e4c9651d4ce772cbab4315711e9e576e7e0
joshua-myers <josh.myers@headway.io>
2022-06-07 03:44:06 +0000

    add user registration

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,2 +1,22 @@
 import type { RegisterForm } from './types.server'
-export async function register(user: RegisterForm) { }
+import { prisma } from './prisma.server'
+import { json } from '@remix-run/node'
+import { createUser } from './users.server'
+
+export const register = async (user: RegisterForm) => {
+  const exists = await prisma.user.count({ where: { email: user.email } })
+  if (exists) {
+    return json({ error: `User already exists with that email` }, { status: 400 })
+  }
+
+  const newUser = await createUser(user);
+  if (!newUser) {
+    return json(
+      {
+        error: `Something went wrong trying to create a new user.`,
+        fields: { email: user.email, password: user.password },
+      },
+      { status: 400 },
+    )
+  }
+}
