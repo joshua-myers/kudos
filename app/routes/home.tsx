6835c85807620600ead48a706d8e5a518482a6d4
joshua-myers <josh.myers@headway.io>
2022-06-10 03:37:29 +0000

    add panel to home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,19 @@
 import { LoaderFunction } from '@remix-run/node'
+import { Layout } from '~/components/layout'
+import { UserPanel } from '~/components/user-panel'
 import { requireUserId } from '~/utils/auth.server'
 
 export const loader: LoaderFunction = async ({ request }) => {
   await requireUserId(request)
-  return null
+  return null // <- A loader always has to return some value, even if that is null
 }
 
 export default function Home() {
-  return <h2>Home Page</h2>
+  return (
+    <Layout>
+      <div className="h-full flex">
+        <UserPanel />
+      </div>
+    </Layout>
+  )
 }
